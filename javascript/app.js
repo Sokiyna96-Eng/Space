228c4b65ae0e6675610173fec1e0e40fd7543244
Sokiyna96-Eng <sokiyna.naser@gmail.com>
2021-03-18 15:24:38 +0200

    added js interactions

---
+++ javascript/app.js
@@ -0,0 +1,15 @@
+var favoritePlanet= prompt("Welcome to the Space webpage, can you provide us with your favorite planet ?");
+
+if(favoritePlanet=="mercury")
+{document.write('<img src="Messenger_s_iridescent_Mercury_pillars.jpg"width=250px>'); alert( "Great choice")
+}else if(favoritePlanet=="mars"){document.write('<img src="OSIRIS_Mars_true_color.jpg"width=250px>'); alert("Cool")}
+
+alert("Please suggst any other planets names")
+
+var con1=" Thank you for visiting our website"
+var con2=", you can contact us on the following email space@gmail.com"
+
+
+document.write(con1 +"  "+ con2)
+
+
