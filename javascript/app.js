0f54177acc2ae99861879afc435f008a219af5e9
Sokiyna96-Eng <sokiyna.naser@gmail.com>
2021-03-21 15:07:17 +0200

    add loop

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,40 @@
 var favoritePlanet= prompt("Welcome to the Space webpage, can you provide us with your favorite planet ?");
 
-if(favoritePlanet=="mercury")
-{document.write('<img src="Messenger_s_iridescent_Mercury_pillars.jpg"width=250px>'); alert( "Great choice")
-}else if(favoritePlanet=="mars"){document.write('<img src="OSIRIS_Mars_true_color.jpg"width=250px>'); alert("Cool")}
+while(favoritePlanet!=='mercury' && favoritePlanet!=='mars')
+{favoritePlanet = prompt('Kindly chose between mercury and mars ')};
 
-alert("Please suggst any other planets names")
+var numberOfpictures = prompt(" How many pictures you would like to see");
+
+var pictures = '';
+var total = '';
+
+if(favoritePlanet=="mercury")
+{ pictures='<img src="Messenger_s_iridescent_Mercury_pillars.jpg"width=250px>'; alert( "Great choice")
+}else if(favoritePlanet=="mars"){pictures='<img src="OSIRIS_Mars_true_color.jpg"width=250px>'; alert("Cool")};
+
+console.log(pictures);
+
+
+for (var i=0; i < numberOfpictures; i++){
+    total += pictures;
+   
+
+    console.log(total, i);
+
+};
+
+i++;
+i= i+1;
+
+document.write(total, i)
+
+
 
 var con1=" Thank you for visiting our website"
 var con2=", you can contact us on the following email space@gmail.com"
 
 
 document.write(con1 +"  "+ con2)
+
 
 
